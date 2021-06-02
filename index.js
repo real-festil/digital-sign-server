2f29f564e01a7241520f9c20f9bb7a70968612ef
Роман Гладышев <romagladyshev@gmail.com>
2021-06-02 16:41:00 +0600

    fix res

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const port = process.env.PORT || 3000
 app.post('/sign', (req, res) => {
   const {pk, file} = req;
   const ds = digitalSign.signFile(pk, file);
-  res.send(ds);
+  res.send(ds || 'error');
 })
 
 app.post('/verify', (req, res) => {
   const {pk, sign, file} = req;
   const ds = digitalSign.verifyFile(pk, sign, file);
-  res.send(ds);
+  res.send(ds || 'error');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening at http://localhost:${port}`)
 })
