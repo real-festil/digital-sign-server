0de88459b039e768d6bb9b52412c2b1f284c4de9
Роман Гладышев <romagladyshev@gmail.com>
2021-06-02 16:50:56 +0600

    testing commit

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const digitalSign= require('digital-signing');
 const port = process.env.PORT || 3000
 
 app.post('/sign', (req, res) => {
-  try {
-    const {pk, file} = req;
-    const ds = digitalSign.signFile(pk, file);
-    res.send(JSON.stringify(ds));
-  } catch (err) {
-    res.send(err.toString());
-  }
+  // try {
+  //   const {pk, file} = req;
+  //   const ds = digitalSign.signFile(pk, file);
+    res.send('hello world');
+  // } catch (err) {
+  //   res.send(err.toString());
+  // }
 })
 
-app.post('/verify', (req, res) => {
-  const {pk, sign, file} = req;
-  const ds = digitalSign.verifyFile(pk, sign, file);
-  res.send(ds || 'error');
-})
+// app.post('/verify', (req, res) => {
+//   const {pk, sign, file} = req;
+//   const ds = digitalSign.verifyFile(pk, sign, file);
+//   res.send(ds || 'error');
+// })
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
