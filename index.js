9ef85f2a806bb495accad2eeed9daa6daba36bf5
Роман Гладышев <romagladyshev@gmail.com>
2021-06-02 16:37:50 +0600

    change get to post

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const digitalSign= require('digital-signing');
 
 const port = process.env.PORT || 3000
 
-app.get('/sign', (req, res) => {
+app.post('/sign', (req, res) => {
   const {pk, file} = req;
   const ds = digitalSign.signFile(pk, file);
   res.send(ds);
 })
 
-app.get('/verify', (req, res) => {
+app.post('/verify', (req, res) => {
   const {pk, sign, file} = req;
   const ds = digitalSign.verifyFile(pk, sign, file);
   res.send(ds);
