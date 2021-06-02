f6a0bdc56453b5260ce0f64ccf77bac3a4c1303f
Роман Гладышев <romagladyshev@gmail.com>
2021-06-02 16:25:58 +0600

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const digitalSign= require('digital-signing');
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/sign', (req, res) => {
   const {pk, file} = req;
